57ed925024b836cc00e45787ee586f3ed8566112
PauloEduCarvalho <pedupp1@gmail.com>
2026-03-05 15:46:05 -0300

    Exception handling

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/exceptions/ResourcesNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscommerce.services.exceptions;
+
+public class ResourcesNotFoundException extends RuntimeException {
+
+    public ResourcesNotFoundException(String msg) {
+        super(msg);
+    }
+}
