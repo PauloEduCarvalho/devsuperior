7adbb3c266bb75b8d1b9c1d104616a2067c2bea7
PauloEduCarvalho <pedupp1@gmail.com>
2026-04-10 17:42:15 -0300

    Saving Order

--- dscommerce/src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -8,6 +8,8 @@ import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class OrderDTO {
 
     private Long id;
@@ -15,7 +17,11 @@ public class OrderDTO {
     private OrderStatus status;
     private ClientDTO client;
     private PaymentDTO payment;
+    @NotEmpty(message = "An order must have at least one item")
     private List<OrderItemDTO> items = new ArrayList<>();
+    
+    public OrderDTO() {
+    }
     
     public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
         this.id = id;
--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -1,12 +1,21 @@
 package com.devsuperior.dscommerce.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.dto.OrderItemDTO;
 import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.OrderItem;
+import com.devsuperior.dscommerce.entities.OrderStatus;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
 import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ResourcesNotFoundException;
 
 @Service
@@ -14,11 +23,43 @@ public class OrderService {
 
     @Autowired
     private OrderRepository repository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+    
+    @Autowired
+    private UserService userService;
     
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
         Order order = repository.findById(id).orElseThrow(
             () -> new ResourcesNotFoundException("Order not found"));
         return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        
+        Order entity = new Order();
+        
+        entity.setMoment(Instant.now());
+        entity.setStatus(OrderStatus.WAITING_PAYMENT);
+        
+        User user = userService.authenticated();
+        entity.setClient(user);
+
+        for (OrderItemDTO itemDTO : dto.getItems()) {
+            Product product = productRepository.getReferenceById(itemDTO.getProductId());
+            OrderItem item = new OrderItem(entity, product, itemDTO.getQuantity(), product.getPrice());
+            entity.getItems().add(item);
+        }
+
+        repository.save(entity);
+        orderItemRepository.saveAll(entity.getItems());
+        
+        return new OrderDTO(entity);
     }
 }
