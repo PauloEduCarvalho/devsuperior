4acb87327aca1408a7de1c02ec5d09f5b1dbcc2c
PauloEduCarvalho <pedupp1@gmail.com>
2026-05-07 14:23:48 -0300

    orm user e role

--- dscatalog-cap1-3.4.4/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ dscatalog-cap1-3.4.4/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -4,10 +4,23 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
@@ -18,7 +31,10 @@ public class User implements Serializable {
 
     private String password;
 
-    
+    @ManyToMany
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))	
     private Set<Role> roles = new HashSet<>();
 
     public User() {
